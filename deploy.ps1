b2d74c5a5be19541beb049fd6e06a1003f02cec4
TeebObeeT <yYLs0CZ2mEay3E73ygWa>
2016-11-24 19:20:47 +0100

    Site update

--- deploy.ps1
+++ deploy.ps1
@@ -1,6 +1,4 @@
-#!/bin/bash
-
-echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+echo "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
 "D:\Program Files\hugo\hugo.exe"
@@ -13,4 +11,4 @@ git commit -m 'Site update'
 
 # Push source and build repos.
 git push origin master
-git subtree push --prefix=public git@github.com:spencerlyon2/hugo_gh_blog.git gh-pages
+git subtree push --prefix=public git@github.com:TeebObeeT/1Mono2Voiles.git gh-pages
